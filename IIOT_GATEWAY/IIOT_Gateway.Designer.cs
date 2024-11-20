9f3001bd9216007159daa7938f122becc9c34a66
Mikhael <mikhaellumbansiantar2@gmail.com>
2024-11-20 23:33:19 +0700

    FIRST PUSH

---
+++ IIOT_GATEWAY/IIOT_Gateway.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace IIOT_GATEWAY
+{
+    partial class IIOT_Gateway
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabPage1 = new TabPage();
+            trDeviceList = new TreeView();
+            pnlHome = new Panel();
+            tabControl1 = new TabControl();
+            btSearch = new Button();
+            tabPage1.SuspendLayout();
+            tabControl1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(btSearch);
+            tabPage1.Controls.Add(pnlHome);
+            tabPage1.Controls.Add(trDeviceList);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(893, 471);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Home";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // trDeviceList
+            // 
+            trDeviceList.Location = new Point(6, 35);
+            trDeviceList.Name = "trDeviceList";
+            trDeviceList.Size = new Size(177, 430);
+            trDeviceList.TabIndex = 0;
+            // 
+            // pnlHome
+            // 
+            pnlHome.Location = new Point(189, 6);
+            pnlHome.Name = "pnlHome";
+            pnlHome.Size = new Size(698, 459);
+            pnlHome.TabIndex = 1;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Location = new Point(12, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(901, 499);
+            tabControl1.TabIndex = 0;
+            // 
+            // btSearch
+            // 
+            btSearch.Location = new Point(6, 6);
+            btSearch.Name = "btSearch";
+            btSearch.Size = new Size(177, 23);
+            btSearch.TabIndex = 1;
+            btSearch.Text = "Search";
+            btSearch.UseVisualStyleBackColor = true;
+            // 
+            // IIOT_Gateway
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(925, 523);
+            Controls.Add(tabControl1);
+            Name = "IIOT_Gateway";
+            Text = "IIOT_Gateway";
+            tabPage1.ResumeLayout(false);
+            tabControl1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabPage tabPage1;
+        private Panel pnlHome;
+        private TreeView trDeviceList;
+        private TabControl tabControl1;
+        private Button btSearch;
+    }
+}
---
+++ IIOT_GATEWAY/IIOT_Gateway.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+namespace IIOT_GATEWAY
+{
+    public partial class IIOT_Gateway : Form
+    {
+        public IIOT_Gateway()
+        {
+            InitializeComponent();
+        }
+    }
+}
